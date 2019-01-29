08b9e8085efb530ea372e13441650022e2ea5b77
Kevin <kevin001rosas@gmail.com>
2019-01-29 15:05:17 -0600

    Api de clientes

---
+++ WeApi_cors/WeApi/Controllers/clientesController.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WeApi.Controllers
+{
+    public class clientesController : ApiController
+    {
+        // GET api/clientes
+        public IHttpActionResult Get()
+        {
+            if (!utilidades.validar_token(Request))
+                return Json("incorrecto");
+
+            string query = "SELECT * from lu_clientes where estado=1;";
+            DataTable tabla = Database.runSelectQuery(query);
+            return Json(utilidades.convertDataTableToJson(tabla));
+        }
+
+        // GET api/clientes/5
+        public IHttpActionResult Get(int id)
+        {
+            if (!utilidades.validar_token(Request))
+                return Json("incorrecto");
+
+            string query = string.Format("SELECT * from lu_clientes where id='{0}'", id);
+            DataTable tabla = Database.runSelectQuery(query);
+            return Json(utilidades.convertDataTableToJson(tabla));
+        }
+
+
+        // POST api/clientes
+        public IHttpActionResult Post([FromBody]Object value)
+        {
+            //Generamos el Datatable para devolver el resultado. 
+            DataTable tabla_resultado = new DataTable();
+            tabla_resultado.Columns.Add("id");
+            tabla_resultado.Rows.Add();
+            tabla_resultado.Rows[0]["id"] = "-1";
+
+            JObject json = JObject.Parse(value.ToString());
+
+            if (!validacion(json))
+                return Json("incorrecto");
+
+            //Actualizamos los datos con un update query. 
+            string insert_query = string.Format("INSERT INTO `lu_clientes` " +
+            "(`nombre`, `id_tipo_de_mascota`, `fecha_de_registro`, `fecha_de_modificacion`) " +
+            "VALUES " +
+            "('{0}', '{1}', {2}, {3}); "
+            , json["nombre"]
+            , json["id_tipo_de_raza"]
+            , "now()"
+            , "now()");
+            //, id);
+
+            //En caso de error, devolverá incorrecto
+            tabla_resultado.Rows[0]["id"] = Database.runInsert(insert_query).ToString();
+            if (tabla_resultado.Rows[0]["id"] == "-1")
+                return Json("incorrecto");
+
+            //Devolcemos la información de la tabla. 
+            return Json(utilidades.convertDataTableToJson(tabla_resultado));
+        }
+
+        private bool validacion(JObject json)
+        {
+            if (!utilidades.validar_token(Request))
+                return false;
+
+            if (json["nombre"].ToString().Trim() == "")
+                return false;
+
+            return true;
+        }
+
+        // PUT api/clientes/5
+        public IHttpActionResult Put(int id, [FromBody]Object value)
+        {
+            JObject json = JObject.Parse(value.ToString());
+
+            if (!validacion(json))
+                return Json("incorrecto");
+
+            //Actualizamos los datos con un update query. 
+            string update_query = string.Format("UPDATE `lu_clientes` " +
+            "set " +
+            "nombre='{0}' " +
+            ",fecha_de_modificacion=now() " +
+            "where id='{1}'"
+            , json["nombre"]
+            , id);
+
+            //Contestamos con el id del nuevo registro.
+            if (Database.runQuery(update_query))
+                return Json("correcto");
+            else
+                return Json("incorrecto");
+        }
+
+        // DELETE api/clientes/5
+        public IHttpActionResult Delete(int id)
+        {
+            if (!utilidades.validar_token(Request))
+                return Json("incorrecto");
+
+            //Actualizamos los datos con un update query. 
+            string update_query = string.Format("UPDATE `lu_clientes` " +
+            "set " +
+            "estado=0 " +
+            ",fecha_de_modificacion=now() " +
+            "where id='{0}'"
+            , id);
+
+            //Contestamos con el id del nuevo registro.
+            if (Database.runQuery(update_query))
+                return Json("correcto");
+            else
+                return Json("incorrecto");
+        }
+    }
+}
